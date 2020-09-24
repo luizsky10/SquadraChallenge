28884b255aaa4918b02facb76d354dddf730ecf4
juninhott9400 <luizhenriquenewage@gmail.com>
2020-09-24 16:16:32 -0300

    Updates

---
+++ precache-manifest.295e55ae857d80079b5843e6709339e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9dceaa82e321d7fefb471ef9d58c5d",
+    "url": "/SquadraChallenge/index.html"
+  },
+  {
+    "revision": "6170821458bac8cc67ee",
+    "url": "/SquadraChallenge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9393d0fd0287b16c1beb",
+    "url": "/SquadraChallenge/static/js/2.e17de22c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SquadraChallenge/static/js/2.e17de22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6170821458bac8cc67ee",
+    "url": "/SquadraChallenge/static/js/main.faca4164.chunk.js"
+  },
+  {
+    "revision": "0af3fddca4e414604b9b",
+    "url": "/SquadraChallenge/static/js/runtime-main.3cc01e19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SquadraChallenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
