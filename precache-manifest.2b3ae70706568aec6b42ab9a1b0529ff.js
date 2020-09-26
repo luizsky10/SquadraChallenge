4605885a0e694b35b4044fd30a6f70306a47d868
juninhott9400 <luizhenriquenewage@gmail.com>
2020-09-26 20:53:32 -0300

    Updates

---
+++ precache-manifest.2b3ae70706568aec6b42ab9a1b0529ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4f0b01047c8aa6defb8cd76fccaa93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dba3b3d98745994d74f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "89e8323844348ccef7a2",
+    "url": "/static/js/2.72512381.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.72512381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dba3b3d98745994d74f",
+    "url": "/static/js/main.6386132d.chunk.js"
+  },
+  {
+    "revision": "a83aa0d6ec0c5b94c3cf",
+    "url": "/static/js/runtime-main.1a8ef1b4.js"
+  },
+  {
+    "revision": "b6d909e3a229dee5c6ad17ee00e2a840",
+    "url": "/static/media/dragonball.b6d909e3.png"
+  }
+]);
